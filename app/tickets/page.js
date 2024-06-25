d3139ae309f00eaf9ac7f2c51912f49f7ab7215c
Mubarak <adesinamubarak123@gmail.com>
2024-06-25 00:00:23 -1100

    create and import ticket lists page

--- app/tickets/page.js
+++ app/tickets/page.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import TicketList from './components/TicketList'
 
 export default function Tickets() {
   return (
     <div>
-        <h2>Tickets</h2>
+        <main>
+          <nav>
+            <div>
+              <h2>Tickets</h2>
+              <p><small>Currently Open Tickets</small></p>
+            </div>
+          </nav>
+
+          <TicketList />
+        </main>
     </div>
   )
 }

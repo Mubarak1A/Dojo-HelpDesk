9636e0be2c2d2c5159d74ebbdfd7bd2ad4759a92
Mubarak <adesinamubarak123@gmail.com>
2024-06-25 21:04:38 -1100

    update redirections

--- app/tickets/components/TicketList.js
+++ app/tickets/components/TicketList.js
@@ -1,11 +1,10 @@
 import Link from "next/link"
 
-const getTickets = async () => {
-    // imitate delay
-  await new Promise(resolve => setTimeout(resolve, 3000))
-
+async function getTickets() {
     const res = await fetch('http://localhost:4000/tickets', {
-        next: 0
+        next: {
+            revalidate: 0 // use 0 to opt out of using cache
+        }
     })
 
     return res.json()
@@ -16,21 +15,29 @@ export default async function TicketList() {
 
     return (
         <>
-            {
-                tickets.map(ticket => (
-                    <div key={ticket.id} className="card my-5">
-                        <Link href={`tickets/${ticket.id}`}>
+            {tickets.map((ticket) => (
+                <div key={ticket.id} className="card my-5">
+                    <Link href={`/tickets/${ticket.id}`}>
+                        <div>
                             <h3>{ticket.title}</h3>
                             <p>{ticket.body.slice(0, 200)}...</p>
-                            <div className={`pill ${ticket.priority}`}>
-                                {ticket.priority} priority
-                            </div>
-                        </Link>
-                    </div>
-                ))
-            }
+                        </div>
+                        {/*
+                        <button
+                            className="btn bg-primary text-white py-1 px-2"
+                            onClick={handleDeleteTicket}
+                        >
+                            Delete Ticket
+                        </button>
+                        */}
+                        <div className={`pill ${ticket.priority}`}>
+                            {ticket.priority} priority
+                        </div>
+                    </Link>
+                </div>
+            ))}
             {tickets.length === 0 && (
-                <p className="text-center">No Available Tickets!!!</p>
+                <p className="text-center">There are no open tickets, yay!</p>
             )}
         </>
     )
--- app/tickets/page.js
+++ app/tickets/page.js
@@ -1,16 +1,20 @@
 import { Suspense } from "react"
 import TicketList from './components/TicketList'
 import Loading from "../loading"
+import Link from "next/link"
 
 export default function Tickets() {
   return (
     <div>
       <main>
-        <nav>
+        <nav className="flex justify-between">
           <div>
             <h2>Tickets</h2>
             <p><small>Currently Open Tickets</small></p>
           </div>
+          <Link href='/tickets/create-ticket' className="btn bg-primary text-white rounded-md p-3">
+            Create New Ticket
+          </Link>
         </nav>
 
         <Suspense fallback={<Loading />}>
